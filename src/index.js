81fd439b3e2864891f8e1a6fc761b6e37ed2d820
Ritik2004 <hritikgaur44@gmail.com>
2024-01-06 14:55:17 +0530

    Created a utility files for functions

--- src/index.js
+++ src/index.js
@@ -8,6 +8,14 @@ dotenv.config({
 
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+        console.log(`Server running on port: ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log("Mongodb connection failed!!!", err)
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,25 @@
+const asyncHandler = (reqHandler) => {
+   (req,res,next)=>{
+    Promise.resolve(reqHandler(req,res,next)).
+    catch((err)=>next(err))
+   }
+}
+
+export {asyncHandler}
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async ()=>{}
+
+
+// const asyncHandler = (fu) => async (req ,res,next)=>{
+//   try{
+//      await fn(req,res,next)
+//   }
+//   catch(error){
+//        res.status(err.code || 500).json({
+//         success:false,
+//         message:err.message
+//        })
+//   }
+// }
+
