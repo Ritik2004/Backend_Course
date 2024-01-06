22d08a875af2f5814f9e77f16ffe228b81cf7999
Ritik2004 <hritikgaur44@gmail.com>
2024-01-06 14:00:10 +0530

    Connected to mongodb in a proper way!

---
+++ src/index.js
@@ -0,0 +1,42 @@
+// require('dotenv').config({path:'./env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js";
+
+dotenv.config({
+    path:'./env'
+})
+
+
+connectDB()
+
+
+
+
+// To connect to database we have used good approaches like
+// we have use iffe , use try and catch block along with
+// async and await 
+
+// import express from "express";
+// const app = express()
+
+
+// ( async =>{
+//     try{
+//      await 
+//      mongoose.connect(`${process.env.
+//         MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.log("ERRR:", error);
+//             throw error
+//         })
+      
+//         app.listen(process.env.PORT, ()=>{
+//          console.log(`App is running om port ${process.env.PORT}`);
+//         })
+
+//     }
+//     catch(error){
+//      console.log("Error:", error)
+//      throw err
+//     }
+// })()
